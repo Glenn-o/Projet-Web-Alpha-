ecd128d4900b614bf4338a455e9a00822867bdd1
Mathieu <dupycournau@gmail.com>
2020-09-21 14:06:49 +0200

    Update de form

--- include/form.php
+++ include/form.php
@@ -2,11 +2,36 @@
 
 $action = GETPOST("action");
 $categorie = GETPOST("categorie");
-$nom = GETPOST("nom");
-$lieu = GETPOST("lieu");
+$name = GETPOST("name");
+$location = GETPOST("location");
+
+if($action == "generate")
+{
+    if($categorie != "" or $nom != "" or $lieu != "")
+    {
+        header('Location: /web/recherche.php?location="'.$lieu.'"?name="'.$name.'
+                "?categorie="'.$categorie.'"');
+    }
+}
+
+
+
+print $categorie;
+
+
+print '<form action="'.$_SERVER['PHP_SELF'].'" method="post"/>';
+print '<input type="hidden" name="action" value="generate"/>';
+print '<input name="categorie"/>';
+print '<input type="button" type="submit" value="Valider"/>';
 
 function GETPOST($champ)
 {
-    return htmlentities($_POST[$champ]) ?? "";
+    if(isset($_POST[$champ]))
+    {
+        return htmlentities($_POST[$champ]);
+    }
+    else {
+        return "";
+    }
 }
 ?>
---
+++ include/searchForm.php
@@ -0,0 +1,31 @@
+<?php
+
+$action = GETPOST("action");
+$categorie = GETPOST("categorie");
+$name = GETPOST("name");
+$location = GETPOST("location");
+
+header('Location: /web/recherche.php?location="'.$lieu.'"?name="'.$name.'
+        "?categorie="'.$categorie.'"');
+
+
+
+print $categorie;
+
+
+print '<form action="'.$_SERVER['PHP_SELF'].'" method="post"/>';
+print '<input type="hidden" name="action" value="generate"/>';
+print '<input name="categorie"/>';
+print '<input type="button" type="submit" value="Valider"/>';
+
+function GETPOST($champ)
+{
+    if(isset($_POST[$champ]))
+    {
+        return htmlentities($_POST[$champ]);
+    }
+    else {
+        return "";
+    }
+}
+?>
