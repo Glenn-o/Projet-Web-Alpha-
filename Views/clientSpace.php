078f131c5cb28e68f588b817ee094a6e645d4472
Glenn Oberlé <oberle.glenn@icloud.com>
2020-09-22 12:05:46 +0200

    index.php fini

--- Views/clientSpace.php
+++ Views/clientSpace.php
@@ -8,7 +8,23 @@
 </head>
 <body>
     <?php require_once "../include/header.php"?>
-    
+    <section id="main">
+        <div id="container1">
+            <div id="info_user">
+
+            </div>
+            <div id="last_ad">
+
+            </div>
+        </div>
+        <div id="container2">
+            <div id="ads">
+
+            </div>
+            <div id="newsletter">
+            </div>
+        </div>
+    </section>
     <?php require_once "../include/footer.php"?>
 </body>
 </html>
