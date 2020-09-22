46bdae8c069d7a2e630e311c9c25cdc0693c5666
Glenn Oberlé <oberle.glenn@icloud.com>
2020-09-22 14:24:32 +0200

    page espace client

--- include/footer.php
+++ include/footer.php
@@ -9,9 +9,9 @@
         <div id="footer_center">
             <h1>Retrouvez nous sur les réseaux sociaux</h1>
             <hr>
-            <a href=""><img src="assets/img/twitter.png" alt="logo twitter" id="logo_twitter"><p>@FOGshop</p></a>
-            <a href=""><img src="assets/img/facebook.png" alt="logo facebook" id="logo_facebook"><p>F.O.G</p></a>
-            <a href=""><img src="assets/img/insta.png" alt="logo insta" id="logo_insta"><p>@FOGshop</p></a>
+            <a href=""><img src="../assets/img/twitter.png" alt="logo twitter" id="logo_twitter"><p>@FOGshop</p></a>
+            <a href=""><img src="../assets/img/facebook.png" alt="logo facebook" id="logo_facebook"><p>F.O.G</p></a>
+            <a href=""><img src="../assets/img/insta.png" alt="logo insta" id="logo_insta"><p>@FOGshop</p></a>
         </div>
         <div id="footer_right">
             <h1>Informations légales</h1>
--- include/header.php
+++ include/header.php
@@ -1,12 +1,12 @@
 <section id="header">
     <header>
-        <a href="index.html"><img src="assets/img/logo.png" alt="logo_fog" id="logo_fog"></a>
+        <a href="../index.php"><img src="../assets/img/logo.png" alt="logo_fog" id="logo_fog"></a>
         <div id="container_menu">
-            <button id="button_ad">creer une annonce +</button>
-            <a href="#"><img src="assets/img/bell.png" alt="logo_bell" id="logo_bell"></a>
-            <a href="#"><img src="assets/img/wrench.png" alt="logo_admin" id="logo_admin"></a>
+            <a href="#"><img src="../assets/img/button.png" alt="button" id="button_ad"></a>
+            <a href="#"><img src="../assets/img/bell.png" alt="logo_bell" id="logo_bell"></a>
+            <a href="#"><img src="../assets/img/wrench.png" alt="logo_admin" id="logo_admin"></a>
             <div id="container_user">
-                <img src="assets/img/user.png" alt="logo_user" id="logo_user">
+                <img src="../assets/img/user.png" alt="logo_user" id="logo_user">
                 <p>Nom user</p>
             </div>
         </div>
