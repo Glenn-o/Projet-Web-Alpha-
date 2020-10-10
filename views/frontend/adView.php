853105423e5576ad16b37d602886b258de994de1
Glenn Oberlé <oberle.glenn@icloud.com>
2020-10-10 17:56:25 +0200

    css facture + css selection de page + css twitter

--- views/frontend/adView.php
+++ views/frontend/adView.php
@@ -43,7 +43,7 @@ $(function(){
     $(".button_seller_phone").click(function(){
         var seller_name = $('.seller_name').val()
         $.post("includes/EmailPhoneSeller.php", {submit: 'phone', name:<?= '"'. $seller['username']  . '"'?>},function(data){
-            $(".button_seller_phone").html(data);
+            $(".button_seller_phone").html("+33" + data);
         })
     });
     $(".button_seller_email").click(function(){
